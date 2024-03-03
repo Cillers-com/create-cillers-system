081769e26ddecfbc1a81c9b8180b95c1eb17b873
André Eriksson <aeriksson@users.noreply.github.com>
2024-03-03 13:41:15 +0100

    migrations: rm

--- migrations/.ipython/profile_default/startup/startup.py
+++
@@ -1,9 +0,0 @@
-
-ipy = get_ipython()  # noqa: F821
-ipy.run_line_magic('load_ext', 'autoreload')
-ipy.run_line_magic('autoreload', '0')
-
-import os  # noqa: E402, F401
-import cillers_cli  # noqa: E402
-
-cillers_cli.log.init()
