cd3601f7d2f8bf19820be2a536aabc3779f5c251
Peder Linder <pederbl@gmail.com>
2024-07-22 07:01:54 +0200

    Checkpoint

--- cillers/app/datastores/couchbase/models/init.py
+++ cillers/app/datastores/couchbase/models/init.py
@@ -53,6 +53,40 @@ def ensure_initialized(conf: config.ClusterChangeConfig, timeout_seconds: int):
                 time.sleep(1)
     assert False
 
+def ensure_metadata_initialized(cluster: Cluster, conf: config.ClusterChangeConfig):
+    bucket_settings = generate_metadata_bucket_settings(conf)
+    cluster = get_cluster(conf)
+    bucket.ensure_provisioned(cluster, bucket_settings)
+    bucket.ensure_scope_exists(cluster, bucket_settings.name, 'cillers_metadata')
+    ensure_change_constructs_initialized(cluster, conf)
+
+def generate_metadata_bucket_settings(conf: config.ClusterChangeConfig) -> BucketSettings:
+    bucket_settings = BucketSettings(
+        name = conf.metadata['bucket'],
+        bucket_type = BucketType.COUCHBASE,
+        ram_quota_mb = conf.metadata_bucket_specs['ram'],
+        num_replicas = conf.metadata_bucket_specs['replicas'],
+        durability_min_level = conf.metadata_bucket_specs['durability']
+    )
+    return bucket_settings
+
+def ensure_change_constructs_initialized(cluster: Cluster, conf: config.ClusterChangeConfig):
+    for key, _ in conf.data_structures:
+        ensure_change_directory_exists(key)
+        ensure_change_collection_exists(cluster, key)
+
+def ensure_change_directory_exists(key: str): 
+    change_file_dir = Path('/root/change/couchbase') / key
+    change_file_dir.mkdir(parents=True, exist_ok=True)
+
+def ensure_change_collection_exists(cluster: Cluster, conf: config.ClusterChangeConfig, key: str):
+    bucket_name = config.metadata['bucket']
+    scope_name = config.metadata['scope']
+    collection_name = f"changes_applied_{key}"
+    if not collection.exists(cluster, bucket_name, scope_name, collection_name):
+        collection.create(cluster, bucket_name, scope_name, collection_name)
+
+
 def wait_until_ready(cluster: Cluster, timeout_seconds: int):
     print("Connecting to Couchbase ...")
     service_types = [
@@ -63,7 +97,6 @@ def wait_until_ready(cluster: Cluster, timeout_seconds: int):
     options = WaitUntilReadyOptions(service_types=service_types)
     for attempt in range(max_retries):
         try:
-            cluster = get(conf, timeout_seconds)
             cluster.wait_until_ready(timeout_option, options)
             print("Couchbase is ready.")
             return
@@ -72,10 +105,12 @@ def wait_until_ready(cluster: Cluster, timeout_seconds: int):
             time.sleep(retry_delay)
     raise Exception("Failed to connnect to Couchbase.")
 
-def get(conf: config.ClusterChangeConfig, timeout_seconds: int):
+def get(protocol: str, host: str, username: str, password: str, timeout_seconds: int):
     max_retries = 20
-    connection_string = generate_connection_string(conf)
-    options = generate_options(conf)
+    connection_string = f"{conf.connection['protocol']}://{conf.connection['host']}"
+    timeout_options = ClusterTimeoutOptions(connect=timeout_seconds)
+    auth_options = PasswordAuthenticator(conf.credentials['username'], conf.credentials['password'])
+    options = ClusterOptions(auth_options, timeout_options=timeout_options)
     for attempt in range(max_retries):
         try:
             cluster = Cluster(connection_string, options)
@@ -86,15 +121,6 @@ def get(conf: config.ClusterChangeConfig, timeout_seconds: int):
             print(f"Cluster connection failed. Retrying ... Error: {e}")
             time.sleep(1)
     assert False
-
-def generate_connection_string(conf: config.ClusterChangeConfig) -> str:
-    return f"{conf.connection['protocol']}://{conf.connection['host']}"
-
-def generate_options(conf: config.ClusterChangeConfig, timeout_seconds) -> ClusterOptions:
-    timeout_options = ClusterTimeoutOptions(connect=timeout_seconds)
-    auth_options = PasswordAuthenticator(conf.credentials['username'], conf.credentials['password'])
-    return ClusterOptions(auth_options, timeout_options=timeout_options)
-
 
 #def is_initialized(client_conf):
 #    max_retries = 30
--- cillers/app/datastores/couchbase/models/metadata.py
+++ cillers/app/datastores/couchbase/models/metadata.py
@@ -4,36 +4,3 @@ from couchbase.bucket import BucketSettings
 from config import config
 from . import bucket, collection 
 
-def ensure_initialized(cluster: Cluster, conf: config.ClusterChangeConfig):
-    bucket_settings = generate_metadata_bucket_settings(conf)
-    cluster = get_cluster(conf)
-    bucket.ensure_provisioned(cluster, bucket_settings)
-    bucket.ensure_scope_exists(cluster, bucket_settings.name, 'cillers_metadata')
-    ensure_change_constructs_initialized(cluster, conf)
-
-def generate_metadata_bucket_settings(conf: config.ClusterChangeConfig) -> BucketSettings:
-    bucket_settings = BucketSettings(
-        name = conf.metadata['bucket'],
-        bucket_type = BucketType.COUCHBASE,
-        ram_quota_mb = conf.metadata_bucket_specs['ram'],
-        num_replicas = conf.metadata_bucket_specs['replicas'],
-        durability_min_level = conf.metadata_bucket_specs['durability']
-    )
-    return bucket_settings
-
-def ensure_change_constructs_initialized(cluster: Cluster, conf: config.ClusterChangeConfig):
-    for key, _ in conf.data_structures:
-        ensure_change_directory_exists(key)
-        ensure_change_collection_exists(cluster, key)
-
-def ensure_change_directory_exists(key: str): 
-    change_file_dir = Path('/root/change/couchbase') / key
-    change_file_dir.mkdir(parents=True, exist_ok=True)
-
-def ensure_change_collection_exists(cluster: Cluster, conf: config.ClusterChangeConfig, key: str):
-    bucket_name = config.metadata['bucket']
-    scope_name = config.metadata['scope']
-    collection_name = f"changes_applied_{key}"
-    if not collection.exists(cluster, bucket_name, scope_name, collection_name):
-        collection.create(cluster, bucket_name, scope_name, collection_name)
-
