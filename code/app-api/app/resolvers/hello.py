2a7d7ecb0bbda660a5fa4fe53debf377b8f8197d
Peder Linder <pederbl@gmail.com>
2024-08-17 10:31:55 +0200

    Fix hello message

--- code/app-api/app/resolvers/hello.py
+++ code/app-api/app/resolvers/hello.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 class Query:
     @strawberry.field(permission_classes=[IsAuthenticated])
     def hello(self) -> types.Message:
-        return types.Message(message="Welcome to Cillers 2!")
+        return types.Message(message="Welcome to Cillers!")
 
     @strawberry.field(permission_classes=[IsAdmin])
     def hello_admin(self) -> types.Message:
